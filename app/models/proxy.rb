53344632c7807397277d5b6775a98dda56f7dad6
Paul Zaczkiewicz <pzaczkiewicz@raybeam.com>
2015-02-23 16:29:57 -0500

    Create Source.fix_source_conflicts

--- app/models/proxy.rb
+++ app/models/proxy.rb
@@ -1,10 +1,11 @@
 class Proxy < ActiveRecord::Base
-  belongs_to :source
+  belongs_to :source, inverse_of: :proxy
   has_many :proxy_performances, dependent: :destroy, inverse_of: :proxy
   has_many :sites, through: :proxy_performances
 
   def restore_or_initialize(host:, port:)
     proxy = self.find_or_initialize(host: host, port: port)
+    proxy.source = nil unless proxy.deleted_at.nil?
     proxy.deleted_at = nil
     proxy
   end
--- app/models/source.rb
+++ app/models/source.rb
@@ -1,18 +1,30 @@
 class Source < ActiveRecord::Base
   has_many :proxies, dependent: :destroy, inverse_of: :source
 
+  SourceConflict = Struct.new(:conflict_exists?)
+
   def add_proxy(host, port)
     proxy = Proxy.restore_or_initialize host: host, port: port
 
-    if !proxy.source.nil?
-      && proxy.source != self
-      && proxy.source.reliability < self.reliability
-
-      proxy.source.decomission_proxy(proxy)
-    end
+    return proxy if self.fix_source_conflicts.conflict_exists?
     self.proxies << proxy
 
     proxy.save
+  end
+
+  def fix_source_conflicts(proxy)
+    conflict = SourceConflict.new false
+    if !proxy.source.nil?
+      && proxy.source != self
+
+      if proxy.source.reliability < self.reliability
+        proxy.source.decomission_proxy(proxy)
+      else
+        self.decomission_proxy(proxy)
+        conflict.conflict_exists? = true
+      end
+    end
+    conflict
   end
 
   def decomission_proxy
