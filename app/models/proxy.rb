262d57f048e4db9678c7d273ea926aef96764d20
Paul Zaczkiewicz <pzaczkiewicz@raybeam.com>
2015-02-23 14:48:09 -0500

    Add Source.add_proxy

--- app/models/proxy.rb
+++ app/models/proxy.rb
@@ -2,5 +2,10 @@ class Proxy < ActiveRecord::Base
   belongs_to :source
   has_many :proxy_performances, dependent: :destroy, inverse_of: :proxy
   has_many :sites, through: :proxy_performances
-  acts_as_paranoid
+
+  def restore_or_initialize(host:, port:)
+    proxy = self.find_or_initialize(host: host, port: port)
+    proxy.deleted_at = nil
+    proxy
+  end
 end
--- app/models/source.rb
+++ app/models/source.rb
@@ -1,3 +1,21 @@
 class Source < ActiveRecord::Base
   has_many :proxies, dependent: :destroy, inverse_of: :source
+
+  def add_proxy(host, port)
+    proxy = Proxy.restore_or_initialize host: host, port: port
+
+    if !proxy.source.nil?
+      && proxy.source != self
+      && proxy.source.reliability < self.reliability
+
+      proxy.source.decomission_proxy(proxy)
+    end
+    self.proxies << proxy
+
+    proxy.save
+  end
+
+  def decomission_proxy
+    raise NotImplementedError, "Implement #{__callee__} in #{self.class.to_s}"
+  end
 end
