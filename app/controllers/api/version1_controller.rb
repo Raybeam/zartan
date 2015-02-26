2ec51443fcf85f661401772fc369663bf828eb4c
David Hollis <dhollis@raybeam.com>
2015-02-26 11:45:21 -0500

    Fill in the result reporting action

--- app/controllers/api/version1_controller.rb
+++ app/controllers/api/version1_controller.rb
@@ -1,11 +1,28 @@
 module Api
   class Version1Controller < ActionController::Base
+    before_filter :set_site
+    
     def get_proxy
       render json: { result: 'success', payload: {} }
     end
     
     def report_result
+      begin
+        @proxy = Proxy.find(params[:proxy_id])
+        if params[:succeeded]
+          @site.proxy_succeeded! @proxy
+        else
+          @site.proxy_failed! @proxy
+        end
+      rescue ActiveRecord::RecordNotFound => e
+        # Swallow not-found-type errors
+      end
       render :nothing
+    end
+    
+    private
+    def set_site
+      @site = Site.find_or_create_by(name: params[:site_name])
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get  'v1/:site_name',           to: 'api/version1#get_proxy', defaults: { older_than: -1 }
-  post 'v1/:site_name/succeeded', to: 'api/version1#report_result', succeeded: true
-  post 'v1/:site_name/failed',    to: 'api/version1#report_result', succeeded: false
+  get  'v1/:site_name',                     to: 'api/version1#get_proxy', defaults: { older_than: -1 }
+  post 'v1/:site_name/:proxy_id/succeeded', to: 'api/version1#report_result', succeeded: true
+  post 'v1/:site_name/:proxy_id/failed',    to: 'api/version1#report_result', succeeded: false
 end
