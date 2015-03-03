8b6b1597a26d6d6f42d6a08eb8247e7245198c87
Paul Zaczkiewicz <pzaczkiewicz@raybeam.com>
2015-03-03 15:17:29 -0500

    Initial code to create a new server

--- Gemfile
+++ Gemfile
@@ -36,6 +36,9 @@ gem 'redis-objects'
 # Use resque to asyncronously run long-running jobs
 gem 'resque'
 
+# Fog supports many different cloud computing services
+gem 'fog'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- app/models/sources/digital_ocean.rb
+++ app/models/sources/digital_ocean.rb
@@ -3,12 +3,57 @@ module Sources
     class << self
       def required_fields
         {
-          username: :string,
-          password: :password,
-          image_name: :string
+          client_id: :string,
+          api_key: :string,
+          image_name: :string,
+          flavor_name: :string
         }
       end
     end
+
+    def decommission_proxy(proxy)
+
+    end
+
+    private
+
+    def connection
+      @connection ||= Fog::Compute.new(
+        :provider => 'DigitalOcean',
+        :digitalocean_api_key => config['api_key'],
+        :digitalocean_client_id => config['client_id']
+      )
+    end
+
+    def server_by_proxy(proxy)
+      connection.servers.select{|s| s.public_ip_address == proxy.host}
+    end
+
+    def image_id
+      connection.images.select{|i| i.name == config['image_name']}.first.id
+    end
+
+    def flavor_id
+      connection.images.select{|f| f.name == config['flavor_name']}.first.id
+    end
+
+    def region_id
+      regions = connection.images.select do |region|
+        ["San Francisco 1", "New York 2", "New York 3"].include? region.name
+      end
+      # TODO: Randomly sample one from the list
+      regions.first.id
+    end
+
+    def create
+      server = connection.servers.create(
+        name: "proxy-#{SecureRandom.uuid}",
+        image_id: image_id,
+        flavor_id: flavor_id,
+        region_id: region_id
+      )
+    end
+
     Source.source_types << self
   end
 end
