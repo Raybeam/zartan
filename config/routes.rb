f00798270f9c578eb3dd0c9ca85571e558a94b76
David Hollis <dhollis@raybeam.com>
2015-03-06 15:52:14 -0500

    Routing fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get  'v1/:site_name',                     to: 'api/version1#get_proxy', defaults: { older_than: -1 }
-  post 'v1/:site_name/:proxy_id/succeeded', to: 'api/version1#report_result', succeeded: true
-  post 'v1/:site_name/:proxy_id/failed',    to: 'api/version1#report_result', succeeded: false
+  get  'v1/:site_name',                     to: 'api/version1#get_proxy', constraints: { site_name: %r{[a-z0-9_.-]+} }
+  post 'v1/:site_name/:proxy_id/succeeded', to: 'api/version1#report_result', constraints: { site_name: %r{[a-z0-9_.-]+} }, succeeded: true
+  post 'v1/:site_name/:proxy_id/failed',    to: 'api/version1#report_result', constraints: { site_name: %r{[a-z0-9_.-]+} }, succeeded: false
   
   resources :sites, only: %i(index show update)
   resources :sources, except: %i(destroy)
