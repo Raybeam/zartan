a5162130eb2d19c636e8a58d5be5904c41bbedd6
David Hollis <dhollis@raybeam.com>
2015-03-06 16:47:03 -0500

    Reformat and better document the routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  get  'v1/:site_name',                     to: 'api/version1#get_proxy', constraints: { site_name: %r{[a-z0-9_.-]+} }
-  post 'v1/:site_name/:proxy_id/succeeded', to: 'api/version1#report_result', constraints: { site_name: %r{[a-z0-9_.-]+} }, succeeded: true
-  post 'v1/:site_name/:proxy_id/failed',    to: 'api/version1#report_result', constraints: { site_name: %r{[a-z0-9_.-]+} }, succeeded: false
+  # API Routes
+  # The constraint is necessary to ensure that :site_name can contain dots
+  constraints site_name: %r{[a-z0-9_.-]+} do
+    get  'v1/:site_name',                     to: 'api/version1#get_proxy'
+    post 'v1/:site_name/:proxy_id/succeeded', to: 'api/version1#report_result', succeeded: true
+    post 'v1/:site_name/:proxy_id/failed',    to: 'api/version1#report_result', succeeded: false
+  end
   
+  # Admin UI Routes
   resources :sites, only: %i(index show update)
   resources :sources, except: %i(destroy)
   resources :proxies, only: %i(show)
@@ -10,5 +15,6 @@ Rails.application.routes.draw do
   get 'config',      to: 'config#show', as: :config
   post 'config/set', to: 'config#set', as: :config_set
   
+  # Map / to the sites page
   root to: 'sites#index'
 end
