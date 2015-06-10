d13525f34448038428cd35a1a19ee7ccc14ac442
David Hollis <dhollis@raybeam.com>
2015-06-10 10:51:00 -0700

    Fix simple syntax error

--- lib/api_logging.rb
+++ lib/api_logging.rb
@@ -5,7 +5,7 @@ class ApiLogging
   
   def api_log(record={})
     tm = Time.now
-    record.merge! { timestamp: tm.strftime('%F %T'), unix_time: tm.to_i }
+    record.merge!(timestamp: tm.strftime('%F %T'), unix_time: tm.to_i)
     ::ApiLogging::API_LOGGER.tagged('api') { |l| l.info(record.to_json) }
   end
 end
