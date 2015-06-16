3c48a8fea73e0d9d76e10a00c0dda1ab8e006fac
Paul Zaczkiewicz <pzaczkiewicz@raybeam.com>
2015-06-16 15:49:18 -0400

    Revert "Add thread ID to api logs"
    This reverts commit 7e95ecf8aacb29195b89b497882c13204211d82c.

--- lib/api_logging.rb
+++ lib/api_logging.rb
@@ -5,11 +5,7 @@ module ApiLogging
   
   def api_log(record={})
     tm = Time.now
-    record.merge!(
-      thread: Thread.current.object_id,
-      timestamp: tm.strftime('%F %T'),
-      unix_time: tm.to_i
-    )
+    record.merge!(timestamp: tm.strftime('%F %T'), unix_time: tm.to_i)
     ::ApiLogging::API_LOGGER.tagged('api') { |l| l.info(record.to_json) }
   end
 end
