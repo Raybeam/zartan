579fb19faaad090a234c258e05707a54521aa828
awong-raybeam <awong@raybeam.com>
2015-07-13 14:27:28 -0700

    Closer. Still a WIP.

--- app/models/sources/linode.rb
+++ app/models/sources/linode.rb
@@ -4,21 +4,23 @@ module Sources
       def required_fields
         super.merge({
           api_key: :password,
+          root_password: :password,
           image_name: :string,
           flavor_name: :string,
+          kernel_name: :string,
           data_center_name: :string
         })
       end
     end
 
-    ID_TYPES = ['image', 'flavor', 'data_center']
+    ID_TYPES = ['image', 'flavor', 'kernel', 'data_center']
 
     # Connect to Linode to make sure our config is valid
     def validate_config!
       valid = false
       begin
         names_to_ids # Force re-evaluation of ids
-        valid = (!image_id.nil? && !flavor_id.nil? && !data_center_id.nil?)
+        valid = (!image_id.nil? && !flavor_id.nil? && !kernel_id.nil? && !data_center_id.nil?)
       rescue Excon::Errors::Unauthorized => e
         add_error "Invalid credentials"
       end
@@ -30,15 +32,16 @@ module Sources
     # server_is_proxy_type?(server)
     # given a server, determine if it is running a proxy
     def server_is_proxy_type?(server)
-      return server.image_id == self.image_id \
-        && server.data_center_id == self.data_center_id \
-        && server.flavor_id == self.flavor_id
-    end
+puts server.inspect
+      # Fog implenentation does not give access to flavor/image/etc data
+      # So doing the most basic of checks here
+      return server.name =~ /linode/
+   end
 
     def connection
       @connection ||= ::Fog::Compute.new(
         :provider => 'Linode',
-        :linode_api_key => config['api_key']
+        :linode_api_key => config['api_key']# MYo6NwkzgE6eYf9X31hB3pI5uWNMLtLOypNgKCvyQqctpumxYV3Yrs1rAb3DvO2S
       )
     end
 
@@ -57,15 +60,16 @@ module Sources
       class_eval <<-RUBY
         # image_id()
         # flavor_id()
+        # kernel_id()
         # data_center_id()
-        # Get the image/flavor/data center id.
+        # Get the image/flavor/kernel/data center id.
         # If the id is undefined, translate image/flavor/data center name to id by
         # connecting to Linode.
         # Returns cached value if already calculated
         # Parameters:
         #   None
         # Returns:
-        #   - a numeric id representing the image/flavor/data center on Linode
+        #   - a numeric id representing the image/flavor/kernel/data center on Linode
         #   - nil if not found
         def #{id_type}_id
           key = '#{id_type}_id'
@@ -76,23 +80,31 @@ module Sources
 
         # retrieve_image_id()
         # retrieve_flavor_id()
+        # retrieve_kernel_id()
         # retrieve_data_center_id()
-        # Retrieve image/flavor/data center id from Linode
+        # Retrieve image/flavor/kernel/data center id from Linode
         # If the id is not found then adds to the source's persistent error log
         # Parameters:
         #   None
         # Returns:
-        #   - a numeric id representing the image/flavor/data center on Linode
+        #   - a numeric id representing the image/flavor/kernel/data center on Linode
         #   - nil if not found
         def retrieve_#{id_type}_id
           name = config['#{id_type}_name']
-          id = connection.#{id_type.pluralize}.select do |i|
-            i.name == name
-          end.first.andand.id
-          if id.nil?
-            names = connection.#{id_type.pluralize}.map(&:name).join(", ")
-            add_error "There is no #{id_type} named \#{name}. " \
-              "Options are: \#{names}"
+#puts name
+          case when '#{id_type}' == 'image'
+            id = connection.image_list.body["DATA"].find { |i| i["LABEL"] == name }["IMAGEID"]
+            add_error "There is no #{id_type} named \#{name}." if id.nil?
+          else
+            id = connection.#{id_type.pluralize}.select do |i|
+              if '#{id_type}' == 'data_center' then i.location == name
+              else i.name == name end
+            end.first.andand.id
+            if id.nil?
+              names = connection.#{id_type.pluralize}.map(&:name).join(", ")
+              add_error "There is no #{id_type} named \#{name}. " \
+                "Options are: \#{names}"
+            end
           end
           id
         end
@@ -107,9 +119,11 @@ module Sources
     def names_to_ids
       config['image_id'] = retrieve_image_id
       config['flavor_id'] = retrieve_flavor_id
+      config['kernel_id'] = retrieve_kernel_id
       config['data_center_id'] = retrieve_data_center_id
       unless config['image_id'].nil? \
         || config['flavor_id'].nil? \
+        || config['kernel_id'].nil? \
         || config['data_center_id'].nil?
 
         self.save
@@ -118,16 +132,39 @@ module Sources
 
 #https://gist.github.com/nesquena/1321948
     def create_server
-      timestamp = Time.now.strftime("%Y.%m.%d-%H.%M.%S")
-      connection.servers.create(
-        name: "proxy-#{timestamp}-#{SecureRandom.uuid}",
-        image: image_id,
-        flavor: flavor_id,
-        data_center: data_center_id
-      )
+      # Linode restricts name to be no more than 50 chars
+      name = "proxy-#{SecureRandom.uuid}"
+      flavor = connection.flavors.get(config['flavor_id'])
+
+      # Create Linode server
+      server_id = connection.linode_create(config['data_center_id'], config['flavor_id'], 1).body['DATA']['LinodeID']
+      connection.linode_update server_id, :label => name
+      server = connection.servers.get(server_id)
+
+      # Create disks
+      swap_id = connection.linode_disk_create(server.id, "#{name}_swap", "ext4", 256).body['DATA']['DiskID']
+      disk_id = connection.linode_disk_createfromimage(server.id, config['image_id'], "#{name}_main", (flavor.disk*1024)-256, config['root_password'], "").body['DATA']['DISKID']
+
+      # Create config
+      config_id = connection.linode_config_create(server.id, config['kernel_id'], name, "#{disk_id},#{swap_id},,,,,,,").body['DATA']['ConfigID']
+
+      # Boot Linode server
+      connection.linode_boot server.id, config_id
+
+#      connection.servers.create(
+#        name: name,
+#        image: image_id,
+#        flavor: flavor_id,
+#        data_center: data_center_id
+#      )
     # Generally get this error when we've hit our limit on # of servers
     rescue Excon::Errors::Forbidden => e
+      connection.servers.get(server_id).destroy if !server_id.nil?
       add_error(JSON.parse(e.response.body)['error_message'])
+      NoServer
+    rescue Exception => e
+      connection.servers.get(server_id).destroy if !server_id.nil?
+      add_error(e.message)
       NoServer
     end
   end
