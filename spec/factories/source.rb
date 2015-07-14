6eef93189b3d2f555f477e7fbf460f0535cd2911
Romer Ibo <ribo@raybeam.com>
2015-07-14 10:15:15 -0700

    added factory

--- spec/factories/source.rb
+++ spec/factories/source.rb
@@ -3,7 +3,7 @@ FactoryGirl.define do
     name "Example Source"
     type "Sources::DigitalOcean"
     max_proxies 5
-    
+
     after(:build) do |source|
       source.config = {
         'client_id' => "DEADBEEF_ID",
@@ -15,6 +15,23 @@ FactoryGirl.define do
         'region_name' => "Nowhere 3",
         'region_id' => 3,
         'proxy_port' => 2341
+      }
+    end
+  end
+
+  factory :joynet_source, class: Sources::Joyent do
+    name "Example Joyent Source"
+    type "Sources::Joyent"
+    max_proxies 5
+
+    after(:build) do |source|
+      source.config = {
+        'user' => "proxy-uuid",
+        'password' => "SECRET_TUNNEL",
+        'datacenter' => "joyentcloud_test_location",
+        'image_id:' => "proxy_image",
+        'package_id' => "flavor_name"
+        'proxy_port' => 1337
       }
     end
   end
