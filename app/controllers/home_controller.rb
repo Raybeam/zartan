974003f064517e064bd7395dfb75ce101b5fb077
David Hollis <dhollis@raybeam.com>
2015-10-29 15:03:26 -0400

    Allow authentication to be disabled.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
   def index
-    if current_user
+    authentication_enabled = GOOGLE_OMNIAUTH.fetch("enabled", true)
+    if current_user or !authentication_enabled
+      # Go straight to /activity if we're either logged in or no auth is required
       redirect_to activity_path
     end
   end
